65677f711ed1e5d14686553a45ad38be901aedb8
Anna Clausen <anna.revall.clausen@gmail.com>
2022-04-18 20:11:26 -0700

    commit tree f006d17eb3f8a2064f075b4ba64cf65be4fd328f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let newArr = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        newArr.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    } 
+
+return newArr;
+}
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number)
+        number--
+    }
+}
